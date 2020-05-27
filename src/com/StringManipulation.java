5169fda66ce44b638d6e632566c6661fd8bd59c9
naini <bsw93naini@gmail.com>
2020-05-27 18:15:53 +0530

    String manipulation

--- src/com/StringManipulation.java
+++ src/com/StringManipulation.java
@@ -6,66 +6,89 @@ import java.util.Map;
 import java.util.Set;
 
 public class StringManipulation {
-	
+
 	//********First Repeated char********
-	
-	public static char firstRepeated(String str) {
-		Set<Character> set = new HashSet<Character>();
-		int i=0;
-		while(i < str.length()) {
-			if(set.contains(str.charAt(i))) {
-				return str.charAt(i);
+	public static Map<Character, Integer> common(String str) {
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for(int i=0; i< str.length(); i++) {
+			if(map.containsKey(str.charAt(i))) {
+				map.put(str.charAt(i), map.get(str.charAt(i))+1);
 			}else {
-				set.add(str.charAt(i));
+				map.put(str.charAt(i), 1);
 			}
-			i++;
 		}
-		return 0;
+
+		return map;
+	}
+	public static void firstRepeated(String str) {
+		Map<Character, Integer> map = common(str);
+		for(int i=0; i<str.length(); i++) {
+			if(map.get(str.charAt(i)) >1) {
+				System.out.println("First repeated char: "+str.charAt(i));
+				break;
+			}
+		}
+
 	}
 	//******First Non-Repeated char******
-	 
+
 	public static void firstNonRepeated(String str) {
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for(int i=0; i< str.length(); i++) {
-			if(map.containsKey(str.charAt(i))) {
-				map.put(str.charAt(i), map.get(str.charAt(i))+1);
-			}else {
-				map.put(str.charAt(i), 1);
-			}
+		Map<Character, Integer> map = common(str);
+
+		for(Map.Entry<Character, Integer> temp : map.entrySet()) { 
+			if(temp.getValue()==1) { 
+				System.out.println("First Non-Repeated char: "+temp.getKey()); 
+				break;
+			} 
 		}
-		for(Map.Entry<Character, Integer> temp : map.entrySet()) {
-			if(temp.getValue() ==1) {
-				System.out.println(temp.getKey());
-			}
-		}		
+
+		/* map.forEach((k,v) -> System.out.println(k +","+ v)); */
 	}
-	
+
 	//******Most Repeated char******
-	public static int mostRepeated(String str) {
+	public static void mostRepeated(String str) {
 		int count=0;
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for(int i=0; i< str.length(); i++) {
-			if(map.containsKey(str.charAt(i))) {
-				map.put(str.charAt(i), map.get(str.charAt(i))+1);
-			}else {
-				map.put(str.charAt(i), 1);
-			}
-		}
+		char ch = 0;
+		Map<Character, Integer> map = common(str);
 		for(Map.Entry<Character, Integer> temp : map.entrySet()) {
 			if(count < temp.getValue()) {
 				count = temp.getValue();
+				ch = temp.getKey();
 			}
 		}
-		return count; 
 		
+		System.out.println("Most repeated char is: "+ch +", " + count + " times");
+	}
+	//******Last non repeated character******
+	public static void lastNonRepeated(String str) {
+		Map<Character, Integer> map = common(str);
+		for(int i=str.length()-1; i>0 ;i--) {
+			if(map.get(str.charAt(i)) == 1) {
+				System.out.println("Last non repeated character : "+str.charAt(i));
+				break;
+			}
+		}
+	}
+
+	//******Last repeated character ******
+	public static void lastRepeated(String str) {
+		Map<Character, Integer> map = common(str);
+		for(int i=str.length()-1; i>0 ;i--) {
+			if(map.get(str.charAt(i)) > 1) {
+				System.out.println("Last repeated character : "+str.charAt(i));
+				break;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "aabbbbccdekjk";
-		char ch = firstRepeated(str);
-		System.out.println("First repeated character : " + ch);
-		System.out.println("Max repeated character : "+mostRepeated(str));
+		firstRepeated(str);
+		firstNonRepeated(str);
+		lastNonRepeated(str);
+		lastRepeated(str);
+		mostRepeated(str);
 
 	}
 
